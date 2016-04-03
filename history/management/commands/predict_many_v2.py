8dc7e3130d070d2a005205889b2826a001d9ff9d
Alexander Blair <alex@snipanet.com>
2016-04-03 13:46:49 -0700

    Update to setup multiprocesssing pools for better speed.

--- history/management/commands/predict_many_v2.py
+++ history/management/commands/predict_many_v2.py
@@ -1,6 +1,10 @@
 from django.core.management.base import BaseCommand
 from history.predict import predict_v2
+from django.conf import settings
 from history.tools import print_and_log
+from multiprocessing import Pool
+
+
 class Command(BaseCommand):
 
     help = 'tests various settings that could make the NN more accurate'
@@ -13,10 +17,12 @@ class Command(BaseCommand):
         
         min_back_options = [100,1000,24*60,24*60*2] 
         # 2/22 - eliminated 10000 
-        # 2/25 -- added 24*50, 24*60*2 because "maybe because NN only contains last 1000 data points (1/3 day).  if only selling happened during taht time, nn will bias towards selling.  duh!"
+        # 2/25 -- added 24*50, 24*60*2 because "maybe because NN only contains last 1000 data points (1/3 day).
+        #   if only selling happened during that time, nn will bias towards selling.  duh!"
         
         granularity_options = [10, 15, 20, 30, 40, 50, 60, 120,240] 
-        # 2/23 notes - results so far: 59 (54% correct) 15 (56% correct) 1 (50% correct) 5 (52% correct).  removing 1,5, adding 30 . 2/23 (pt 2) -- added 119, 239
+        # 2/23 notes - results so far: 59 (54% correct) 15 (56% correct) 1 (50% correct) 5 (52% correct).
+        #   removing 1,5, adding 30 . 2/23 (pt 2) -- added 119, 239
         # 2/24 notes -- removed 120,240, added 20, 40, 45
         # 2/25 notes -- added 10, 50, removed 45
         # 2/25 -- added 120,240 back in to retest in light of recent bugs
@@ -35,7 +41,8 @@ class Command(BaseCommand):
         momentum_options = [0.1]
         
         learningrate_options = [0.05] 
-        #2/22 -- elimated 0.005, 0.01, adding 0.03 and 0.1 today.  #2/23 - 0.1 (54% correct) 0.05 (55% correct) 0.03 (54% correct) .  eliminating everything but 0.05 so i can test more #datasetinput_options
+        # 2/22 -- elimated 0.005, 0.01, adding 0.03 and 0.1 today.  #2/23 - 0.1 (54% correct) 0.05 (55% correct) 0.03
+        # (54% correct) .  eliminating everything but 0.05 so i can test more #datasetinput_options
         
         weightdecay_options = [0.0] 
         # 2/22 -- eliminated 0.1,0.2
@@ -43,33 +50,52 @@ class Command(BaseCommand):
         recurrent_options = [True] 
         # 2/23 notes - 0 (52% correct) 1 (55% correct), removed false
 
-        timedelta_back_in_granularity_increments_options = [10,30,60,100,1000]  #sets how far apart (in granularity increments) the datasets are 
-        
+        # sets how far apart (in granularity increments) the data sets are
+        timedelta_back_in_granularity_increments_options = [10,30,60,100,1000]
+
+        pool = Pool(settings.NUM_THREADS)
+
+        print("Starting V2 Run")
         for ticker in ticker_options:
             for hidden_layers in hidden_layer_options:
                 for min_back in min_back_options:
                     for epochs in epoch_options:
-                        for  granularity in granularity_options: 
+                        for granularity in granularity_options:
                             for datasetinputs in datasetinput_options:
                                 for bias in bias_options:
                                     for momentum in momentum_options:
                                         for learningrate in learningrate_options:
                                             for weightdecay in weightdecay_options:
                                                 for recurrent in recurrent_options:
-                                                    for timedelta_back_in_granularity_increments in timedelta_back_in_granularity_increments_options:
-                                                        try:
-                                                            predict_v2(ticker,
-                                                                hidden_layers=hidden_layers,
-                                                                NUM_MINUTES_BACK=min_back,
-                                                                NUM_EPOCHS=epochs,
-                                                                granularity_minutes=granularity,
-                                                                datasetinputs=datasetinputs,
-                                                                learningrate=learningrate,
-                                                                bias=bias,
-                                                                momentum=momentum,
-                                                                recurrent=recurrent,
-                                                                weightdecay=weightdecay,
-                                                                timedelta_back_in_granularity_increments=timedelta_back_in_granularity_increments)
-                                                        except Exception as e:
-                                                            print_and_log("(p)"+str(e))
-                                                    
+                                                    for timedelta_back_in_granularity_increments in \
+                                                            timedelta_back_in_granularity_increments_options:
+                                                        pool.apply_async(self._do_classifier_test, args=(
+                                                            ticker, hidden_layers, min_back, epochs, granularity,
+                                                            datasetinputs,
+                                                            learningrate, bias, momentum, recurrent, weightdecay,
+                                                            timedelta_back_in_granularity_increments
+                                                        ))
+        print("All V2 jobs queued")
+        pool.close()
+        pool.join()
+        print("V2 Run Complete")
+
+    @staticmethod
+    def _do_classifier_test(ticker, hidden_layers, min_back, epochs, granularity, datasetinputs,
+                            learningrate, bias, momentum, recurrent, weightdecay,
+                            timedelta_back_in_granularity_increments):
+        try:
+            predict_v2(ticker,
+                       hidden_layers=hidden_layers,
+                       NUM_MINUTES_BACK=min_back,
+                       NUM_EPOCHS=epochs,
+                       granularity_minutes=granularity,
+                       datasetinputs=datasetinputs,
+                       learningrate=learningrate,
+                       bias=bias,
+                       momentum=momentum,
+                       recurrent=recurrent,
+                       weightdecay=weightdecay,
+                       timedelta_back_in_granularity_increments=timedelta_back_in_granularity_increments)
+        except Exception as e:
+            print_and_log("(p)" + str(e))
