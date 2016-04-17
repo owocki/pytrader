e259875fd5f46ae51d220a4b3ef1212e0e7aee20
Kevin Owocki <kevin@occipital.com>
2016-04-17 14:32:07 -0600

    updates network_created_on field

--- history/models.py
+++ history/models.py
@@ -570,7 +570,7 @@ class SocialNetworkMention(AbstractedTesterClass):
     network_name = models.CharField(max_length=30, db_index=True)
     network_username = models.CharField(max_length=100)
     network_id = models.CharField(default=0, max_length=100, db_index=True)
-    network_created_on = models.DateTimeField(auto_now_add=True)
+    network_created_on = models.DateTimeField()
     symbol = models.CharField(max_length=30, db_index=True)
     text = models.TextField()
     sentiment_polarity = models.FloatField(default=0.00)
