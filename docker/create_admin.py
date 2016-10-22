58a809cb57274ca847212c4a99978d439fc728c2
Zalan Blenessy <zalan.blenessy@gmail.com>
2016-10-22 18:32:57 +0200

    create_admin.py: made flake8 happy

--- docker/create_admin.py
+++ docker/create_admin.py
@@ -2,7 +2,7 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pypolo.settings")
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # noqa: E402
 
 username = os.environ.get("PYTRADER_USER", 'trader')
 password = os.environ.get("PYTRADER_PASSWORD", 'trader')
